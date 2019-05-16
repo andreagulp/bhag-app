db4ca094ec8ee773a311a727141716f80131841e
andreagulp <andreagulp@users.noreply.github.com>
2019-05-16 12:19:53 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Have We Reached Our 200K BHAG Yet?`,
     description: `Go Go`,
     author: `kaizen`,
-    actuals: `36533`,
+    actuals: `50000`,
     bhag: `200000`
   },
   plugins: [
