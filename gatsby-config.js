ac18b92cbe0dc73bfb134affc211db2c9a680a3d
andreagulp <andreagulp@users.noreply.github.com>
2019-12-06 10:21:58 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Have We Reached Our 200K BHAG Yet?`,
     description: `Go Go`,
     author: `kaizen`,
-    actuals: `120000`,
+    actuals: `197124`,
     bhag: `200000`
   },
   plugins: [
